cb7c2e74332d733c117214890fcb9b060272ebf6
Albert Lin <Albertkuilin@gmail.com>
2022-12-04 21:54:57 -0500

    made avatar value representation 0 vector

--- world_model/utils.py
+++ world_model/utils.py
@@ -6,38 +6,8 @@ def convert_obs_to_multilabel(obs):
     multilabel[..., 0] = torch.sum(obs, dim=-1) < 1
     return multilabel
 
-def convert_multilabel_to_emb(multilabel, text, world_model):
-    query = world_model.emma.sprite_emb(torch.arange(17, device=world_model.device)) # 17 x sprite_emb_dim
-
-    # Attention-based text representation        
-    key = world_model.emma.txt_key(text)
-    key_scale = world_model.emma.scale_key(text) # (num sent, sent_len, 1)
-    key = key * key_scale
-    key = torch.sum(key, dim=1) # num sent x key_emb_dim
-    
-    # use world_model custom value
-    value = world_model.txt_val(text)
-    val_scale = world_model.scale_val(text)
-    value = value * val_scale
-    value = torch.sum(value, dim=1) # num sent x val_emb_dim
-
-    kq = query @ key.t() # dot product attention (17 x num sent)
-    mask = (kq != 0) # keep zeroed-out entries zero
-    kq = kq / world_model.emma.attn_scale # scale to prevent vanishing grads
-    weights = F.softmax(kq, dim=-1) * mask # (17 x num sent)
-    values = torch.mean(weights.unsqueeze(-1) * value, dim=-2) # (17 x val_emb_dim)
-
-    keys = query*multilabel[..., None] # (10 x 10 x 17 x key_emb_dim)
-    key = torch.sum(keys, dim=-2) / torch.sum(multilabel, dim=-1, keepdim=True)
-
-    values[15:17] = query[15:17]
-    values = values*multilabel[..., None] # (10 x 10 x 17 x val_emb_dim)
-    value = torch.sum(values, dim=-2) / torch.sum(multilabel, dim=-1, keepdim=True)
-
-    return torch.cat((multilabel, value), dim=-1)
-
 def ground(text, emma):
-    query = emma.sprite_emb(torch.arange(17, device=text.device)) # 17 x sprite_emb_dim
+    query = emma.sprite_emb(torch.arange(17, device=emma.device)) # 17 x sprite_emb_dim
 
     # Attention-based text representation        
     key = emma.txt_key(text)
@@ -51,6 +21,20 @@ def ground(text, emma):
     weights = F.softmax(kq, dim=-1) * mask # (17 x num sent)
     
     return weights
+
+def convert_multilabel_to_emb(multilabel, text, world_model):
+    value = world_model.txt_val(text)
+    val_scale = world_model.scale_val(text)
+    value = value * val_scale
+    value = torch.sum(value, dim=1) # num sent x val_emb_dim
+
+    weights = ground(text, world_model)
+    entity_values = torch.mean(weights.unsqueeze(-1) * value, dim=-2) # (17 x val_emb_dim)
+    entity_values[15:17] = torch.tensor([0], device=world_model.device)
+    entity_values = entity_values*multilabel[..., None] # (10 x 10 x 17 x val_emb_dim)
+    entity_value = torch.sum(entity_values, dim=-2)
+
+    return torch.cat((multilabel, entity_value), dim=-1)
 
 def key_attend(text, emma):
     # Attention-based text representation        
