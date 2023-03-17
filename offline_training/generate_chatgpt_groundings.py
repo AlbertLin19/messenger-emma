5921c3042edebddebdc1ae21bbccb4c8b672035a
Albert Lin <Albertkuilin@gmail.com>
2023-03-17 16:37:07 -0700

    implemented for original text jsons

--- offline_training/generate_chatgpt_groundings.py
+++ offline_training/generate_chatgpt_groundings.py
@@ -1,4 +1,5 @@
-import pickle
+import os
+import json
 import openai
 
 from tqdm import tqdm
@@ -11,7 +12,7 @@ MOVEMENT_TYPES = {
 }
 
 PROMPT_TEMPLATE = """
-This is a list of entity names and their corresponding IDs:
+This is a list of entities and their corresponding IDs:
 airplane: 2
 mage: 3
 dog: 4
@@ -25,7 +26,7 @@ robot: 11
 queen: 12
 sword: 13
 
-This is a list of movement names and their corresponding IDs:
+This is a list of movement types and their corresponding IDs:
 chasing: 0
 fleeing: 1
 stationary: 2
@@ -33,57 +34,56 @@ stationary: 2
 Respond strictly in the following format. Do not add anything else.
 entity ID, movement ID
 
-What is the entity ID and movement ID that corresponds to the following description?
+For example, an appropriately formatted response is:
+2, 0
+
+What is the entity ID and movement ID that correspond to the following description?
 %s
 """
 
-DATASET_PATH = "datasets/stage_2_same_worlds_dataset.pickle"
-NEW_DATASET_PATH = "datasets/stage_2_same_worlds_dataset_with_chatgpt_groundings.pickle"
+TEXT_DIR = "../messenger/envs/texts"
+TEXT_FILES = ["text_train.json", "text_val.json", "text_test.json"]
 
-with open(DATASET_PATH, "rb") as f:
-    dataset = pickle.load(f)
+for text_file in TEXT_FILES:
+    print(text_file)
 
-for data_name, data in dataset.items():
-    print(data_name)
-    manuals = data["manuals"]
-    ground_truths = data["ground_truths"]
-    chatgpt_groundings = []
-
-    n_entity_total = 0
+    text_tuples = []
+    with open(os.path.join(TEXT_DIR, text_file), "r") as f:
+        texts = json.load(f)
+        
+        for entity_type, entity_texts in texts.items():
+            if entity_type not in ENTITY_IDS.keys():
+                continue
+            for role_type, role_texts in entity_texts.items():
+                for movement_type, movement_texts in role_texts.items():
+                    if movement_type not in MOVEMENT_TYPES.keys():
+                        continue
+                    for movement_text in movement_texts:
+                        text_tuples.append((ENTITY_IDS[entity_type], MOVEMENT_TYPES[movement_type], movement_text))
+    
+    chatgpt_grounding = {}
     n_entity_correct = 0
-    n_mvmt_total = 0
     n_mvmt_correct = 0
+    for i in tqdm(range(len(text_tuples))):
+        entity_id_gt, movement_id_gt, text = text_tuples[i]
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", 
+            messages=[{"role": "user", "content": PROMPT_TEMPLATE % text}]
+        )["choices"][0]["message"]["content"]
+        entity_id, movement_id = (response + ",").replace(" ", "").split(",")[:2]
+        entity_id = int(entity_id) if entity_id.isdigit() else -1
+        entity_id = entity_id if entity_id in ENTITY_IDS.values() else -1
+        movement_id = int(movement_id) if movement_id.isdigit() else -1
+        movement_id = movement_id if movement_id in MOVEMENT_TYPES.values() else -1
 
-    for i in tqdm(range(len(manuals))):
-        manual = manuals[i]
-        ground_truth = ground_truths[i]
-        chatgpt_grounding = []
+        if entity_id == entity_id_gt:
+            n_entity_correct += 1
+        if movement_id == movement_id_gt:
+            n_mvmt_correct += 1
+        chatgpt_grounding[text] = (entity_id, movement_id)
+    
+    print(f"entities correct: {n_entity_correct}/{len(text_tuples)}")
+    print(f"mvmts correct: {n_mvmt_correct}/{len(text_tuples)}")
 
-        for j in range(len(manual)):
-            response = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo", 
-                messages=[{"role": "user", "content": PROMPT_TEMPLATE % manual[j]}]
-            )["choices"][0]["message"]["content"]
-            entity_id, movement_id = (response + ",").replace(" ", "").split(",")[:2]
-            entity_id = int(entity_id) if entity_id.isdigit() else -1
-            entity_id = entity_id if entity_id in ENTITY_IDS.values() else -1
-            movement_id = int(movement_id) if movement_id.isdigit() else -1
-            movement_id = movement_id if movement_id in MOVEMENT_TYPES.values() else -1
-
-            if ENTITY_IDS[ground_truth[j][0]] == entity_id:
-                n_entity_correct += 1
-            if MOVEMENT_TYPES[ground_truth[j][1]] == movement_id:
-                n_mvmt_correct += 1
-            n_entity_total += 1
-            n_mvmt_total += 1
-
-            chatgpt_grounding.append((entity_id, movement_id))
-
-        chatgpt_groundings.append(chatgpt_grounding)
-
-    data["chatgpt_groundings"] = chatgpt_groundings
-    print(f"entities correct: {n_entity_correct}/{n_entity_total}")
-    print(f"mvmts correct: {n_mvmt_correct}/{n_mvmt_total}")
-
-with open(NEW_DATASET_PATH, "wb") as f:
-    pickle.dump(dataset, f)
+    with open(os.path.join(TEXT_DIR, "chatgpt_grounding_for_" + text_file), "w") as f:
+        json.dump(chatgpt_grounding, f)
