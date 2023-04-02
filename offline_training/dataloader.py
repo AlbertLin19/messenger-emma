b4695ad911e39944c21e5d4d29a50b4169ca32a0
Albert Lin <Albertkuilin@gmail.com>
2023-04-02 18:52:19 -0400

    made mode and batch_size an instance var

--- offline_training/dataloader.py
+++ offline_training/dataloader.py
@@ -45,7 +45,7 @@ def get_ground_truth(dataset, split, i):
 # takes in custom dataset (which has its own custom keywords) 
 # and outputs in terms of Messenger keywords
 class DataLoader:
-    def __init__(self, dataset, split, max_rollout_length):
+    def __init__(self, dataset, split, max_rollout_length, mode, batch_size):
         self.n_rollouts = len(dataset["rollouts"][split]["manual_idxs"])
         self.rollout_lengths = np.array([min(len(sequence), max_rollout_length) for sequence in dataset["rollouts"][split]["grid_sequences"]])
         self.rollout_probs = (self.rollout_lengths-1)/np.sum(self.rollout_lengths-1)
@@ -62,14 +62,17 @@ class DataLoader:
             self.reward_sequences_array[i, :self.rollout_lengths[i]] = dataset["rollouts"][split]["reward_sequences"][i][:max_rollout_length]
             self.grid_sequences_array[i, :self.rollout_lengths[i]] = dataset["rollouts"][split]["grid_sequences"][i][:max_rollout_length]
         print("n_rollouts:", self.n_rollouts, "; max_rollout_length:", np.max(self.rollout_lengths))
+
+        self.mode = mode 
+        assert mode in ["random", "static"]
+        self.batch_size = batch_size
     
-    def reset(self, mode, batch_size):
-        self.mode = mode
+    def reset(self):
         if self.mode == "random":
             # randomly sample batch_size rollouts from data (with probability proportional to their lengths) and keep track of their lengths
-            self.indices = np.random.choice(self.n_rollouts, size=batch_size, p=self.rollout_probs)
+            self.indices = np.random.choice(self.n_rollouts, size=self.batch_size, p=self.rollout_probs)
             # randomly sample starting state for each rollout
-            self.timesteps = (np.random.rand(batch_size)*(self.rollout_lengths[self.indices] - 1)).astype(int)
+            self.timesteps = (np.random.rand(self.batch_size)*(self.rollout_lengths[self.indices] - 1)).astype(int)
         else:
             raise NotImplementedError
         return self.manuals_array[self.indices], self.ground_truths_array[self.indices], self.grid_sequences_array[self.indices, self.timesteps]
