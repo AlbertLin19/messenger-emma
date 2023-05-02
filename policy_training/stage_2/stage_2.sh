682e282a59cd3adfe9820a1b4a2ca3a24fb8c4ed
Albert Lin <Albertkuilin@gmail.com>
2023-05-01 21:04:07 -0400

    added splits path arg to slurms

--- policy_training/stage_2/stage_2.sh
+++ policy_training/stage_2/stage_2.sh
@@ -4,7 +4,7 @@ ENTITY="aklin" # replace with your wandb entity
 LOG_GROUP="emma_s2" # group to log runs in wandb
 SLURM_ARGS="-A pnlp" # replace with your slurm args
 
-CMD="python ../train.py --stage 2 --lr 0.00005 --update_timestep 64 --optimizer Adam --weight_decay 0.0 --max_time 47.75 --max_steps 64 --gamma 0.99 --freeze_attention --entity $ENTITY --log_group $LOG_GROUP"
+CMD="python ../train.py --splits_path ../../offline_training/custom_dataset/data_splits_final.json --stage 2 --lr 0.00005 --update_timestep 64 --optimizer Adam --weight_decay 0.0 --max_time 47.75 --max_steps 64 --gamma 0.99 --freeze_attention --entity $ENTITY --log_group $LOG_GROUP"
 
 mkdir -p output
 mkdir -p slurm
